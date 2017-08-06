dc1113df92a5a9c4f88886b3f38bb7ab26b35be4
Tonny <tonnymul0908@gmail.com>
2017-08-07 03:43:02 +0700

    Settings
    SettingFragment
    * fix untuk menampilkan interval berupa sec, bukan ms
    
    fragment_setting
    * mengubah ms menjadi sec

--- app/src/main/java/layout/SettingFragment.java
+++ app/src/main/java/layout/SettingFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -28,7 +29,7 @@ import java.util.Date;
 
 import static com.inspira.gms.IndexInternal.global;
 
-public class SettingFragment extends Fragment implements View.OnClickListener{
+public class SettingFragment extends Fragment implements View.OnClickListener, View.OnTouchListener{
     EditText edtInterval, edtRadius;
     TextView tvStartTracking, tvEndTracking;
     Spinner spTracking;
@@ -63,6 +64,8 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
         btnUpdate = (Button) getView().findViewById(R.id.btnUpdate);
         getActivity().setTitle("Settings");
 
+        edtRadius.setOnTouchListener(this);  //added by Tonny @07-Aug-2017
+        edtInterval.setOnTouchListener(this);  //added by Tonny @07-Aug-2017
         tvStartTracking.setOnClickListener(this);
         tvEndTracking.setOnClickListener(this);
         btnUpdate.setOnClickListener(this);
@@ -103,11 +106,29 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
         }else if(id == R.id.tvEndTracking){
             timetype = 2;
             tp.show();
-        }else if(id == R.id.btnUpdate){
+        }else if(id == R.id.btnUpdate) {
             String actionUrl = "Settings/setSettings/";
-            new updateSettings().execute( actionUrl );
+            new updateSettings().execute(actionUrl);
             LibInspira.ShowShortToast(getContext(), "Saving...");
         }
+//        }else if(id == R.id.edtInterval){
+//            edtInterval.setText(edtInterval.getText().toString().replace(",",""));
+//        }else if(id == R.id.edtRadius){
+//            edtRadius.setText(edtInterval.getText().toString().replace(",",""));
+//        }
+    }
+
+    //added by Tonny @07-Aug-2017 menggunakan onTouch karena onClick tidak berjalan dengan baik untuk
+    //menjalankan pengecekan pada edtInterval dan edtRadius
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int id = v.getId();
+        if(id == R.id.edtInterval){
+            edtInterval.setText(edtInterval.getText().toString().replace(",",""));
+        }else if(id == R.id.edtRadius){
+            edtRadius.setText(edtInterval.getText().toString().replace(",",""));
+        }
+        return false;
     }
 
     private class checkSettings extends AsyncTask<String, Void, String> {
@@ -138,7 +159,14 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
                                 LibInspira.setShared(global.settingpreferences, global.settings.tracking, obj.getString("tracking"));
                                 LibInspira.setShared(global.settingpreferences, global.settings.jam_awal, obj.getString("jam_awal"));
                                 LibInspira.setShared(global.settingpreferences, global.settings.jam_akhir, obj.getString("jam_akhir"));
-                                edtInterval.setText(LibInspira.delimeter(LibInspira.getShared(global.settingpreferences, global.settings.interval, "0"), true));
+                                Integer ms = Integer.parseInt(LibInspira.getShared(global.settingpreferences, global.settings.interval, "0"));
+                                Integer sec = 0;
+                                if (ms > 0){
+                                    sec = ms/1000;
+                                }
+                                //modified by Tonny @07-Aug-2017
+                                //edtInterval.setText(LibInspira.delimeter(LibInspira.getShared(global.settingpreferences, global.settings.interval, "0"), true));
+                                edtInterval.setText(LibInspira.delimeter(sec.toString()));
                                 edtRadius.setText(LibInspira.delimeter(LibInspira.getShared(global.settingpreferences, global.settings.radius, "0"), true));
                                 if(obj.getString("tracking").equals("GPS Only")){
                                     spTracking.setSelection(0);
@@ -171,7 +199,10 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
 
     private class updateSettings extends AsyncTask<String, Void, String> {
         JSONObject jsonObject;
-        String interval = edtInterval.getText().toString();
+        //remarked by Tonny @07-Aug-2017
+        //String interval = edtInterval.getText().toString();
+        Integer ms = Integer.parseInt(edtInterval.getText().toString()) * 1000;  //modified by Tonny @07-Aug-2017 diubah menjadi millisecond (ms)
+        String interval = ms.toString();
         String radius = edtRadius.getText().toString();
         String tracking = spTracking.getSelectedItem().toString();
         String jam_awal = tvStartTracking.getText().toString();
