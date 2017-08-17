efe31c44f520a3c5d28c0c28eaa90d224d450b3b
Tonny <tonnymul0908@gmail.com>
2017-08-17 11:59:12 +0700

    Minor additional
    DashboardInternalFragment
    * Menambahkan navigasi ke stockmonitoring
    
    (+)StockMonitoritFragment
    
    (+)fragment_stock_monitoring

---
+++ app/src/main/java/layout/StockMonitoringFragment.java
@@ -0,0 +1,129 @@
+/******************************************************************************
+    Author           : ADI
+    Description      : dashboard untuk internal
+    History          :
+
+******************************************************************************/
+package layout;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.inspira.gms.LibInspira;
+import com.inspira.gms.R;
+
+//import android.app.Fragment;
+
+public class StockMonitoringFragment extends Fragment implements View.OnClickListener{
+    public StockMonitoringFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_stock_monitoring, container, false);
+        getActivity().setTitle("Stock Monitoring");
+        return v;
+    }
+
+    /*****************************************************************************/
+    //OnAttach dijalankan pada saat fragment ini terpasang pada Activity penampungnya
+    /*****************************************************************************/
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    //untuk mapping UI pada fragment, jangan dilakukan pada OnCreate, tapi dilakukan pada onActivityCreated
+    @Override
+    public void onActivityCreated(Bundle bundle){
+        super.onActivityCreated(bundle);
+        //added by Tonny @17-Aug-2017
+        getView().findViewById(R.id.btnPosisiStok).setOnClickListener(this);
+        getView().findViewById(R.id.btnPosisiStokRandom).setOnClickListener(this);
+        getView().findViewById(R.id.btnRandomPerBarang).setOnClickListener(this);
+        getView().findViewById(R.id.btnRandomPerLokasi).setOnClickListener(this);
+        getView().findViewById(R.id.btnMutasiStok).setOnClickListener(this);
+        getView().findViewById(R.id.btnKartuStok).setOnClickListener(this);
+        getView().findViewById(R.id.btnRekapGlobalStok).setOnClickListener(this);
+        getView().findViewById(R.id.btnRekapCutSize).setOnClickListener(this);
+        getView().findViewById(R.id.btnRekapUkuranVariasi).setOnClickListener(this);
+        getView().findViewById(R.id.btnLaporanRekapCutSize).setOnClickListener(this);
+        getView().findViewById(R.id.btnRekapStok).setOnClickListener(this);
+        getView().findViewById(R.id.btnLapRekapStokPerJenisGrade).setOnClickListener(this);
+        ///
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if(id==R.id.btnPosisiStok)
+        {
+
+        }
+        else if(id==R.id.btnPosisiStokRandom)
+        {
+
+        }
+        else if(id==R.id.btnRandomPerBarang)
+        {
+
+        }
+        else if(id==R.id.btnRandomPerLokasi)
+        {
+
+        }
+        else if(id==R.id.btnMutasiStok)
+        {
+
+        }
+        else if(id==R.id.btnKartuStok)
+        {
+
+        }
+        else if(id==R.id.btnRekapGlobalStok)
+        {
+
+        }
+        else if(id==R.id.btnRekapCutSize)
+        {
+
+        }
+        else if(id==R.id.btnRekapUkuranVariasi)
+        {
+
+        }
+        else if(id==R.id.btnLaporanRekapCutSize)
+        {
+
+        }
+        else if(id==R.id.btnRekapStok)
+        {
+
+        }
+        else if(id==R.id.btnLapRekapStokPerJenisGrade)
+        {
+
+        }
+    }
+}
