c88980c2d5e1de0fb1c52d3cf17c4f0d7f62019a
Tonny <tonnymul0908@gmail.com>
2017-08-23 18:54:11 +0700

    Random per Barang (salah nama file)
    StockPosisiRandomPerBarang --> StockRandomPerBarangFragment
    * Create New

---
+++ app/src/main/java/layout/StockRandomPerBarangFragment.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+    Author           : ADI
+    Description      : dashboard untuk internal
+    History          :
+
+******************************************************************************/
+package layout;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inspira.gms.R;
+
+//import android.app.Fragment;
+
+public class StockRandomPerBarangFragment extends StockPosisiFragment implements View.OnClickListener{
+    public StockRandomPerBarangFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_choose, container, false);
+        getActivity().setTitle("Stock Random per Barang");
+        return v;
+    }
+
+
+    /*****************************************************************************/
+    //OnAttach dijalankan pada saat fragment ini terpasang pada Activity penampungnya
+    /*****************************************************************************/
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    //added by Tonny @15-Jul-2017
+    //untuk mapping UI pada fragment, jangan dilakukan pada OnCreate, tapi dilakukan pada onActivityCreated
+    @Override
+    public void onActivityCreated(Bundle bundle){
+        actionUrl = "Stock/getStockRandomPerBarang/";
+        super.onActivityCreated(bundle);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
